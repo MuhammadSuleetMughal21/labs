47f01c10f6ed05723ef340184dda6e07d3aafdee
MuhammadSuleetMughal21 <107101885+MuhammadSuleetMughal21@users.noreply.github.com>
2022-06-19 14:48:19 +0500

    Add files via upload

---
+++ food_conn.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package food;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ryan
+ */
+public class food_conn {
+    Connection conn = null;
+    
+    public static Connection food_connection(){
+        //"org.sqlite.JDBC"
+        try{
+            // Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            Connection conn = DriverManager.getConnection("jdbc:derby://localhost:1527/food", "suleet", "123");
+            System.out.println("connected");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+            //System.out.println("connected");
+            return null;
+        }
+        
+    }
+}
